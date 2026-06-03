22d0081c0bf4b614bdbb49009cc7f9622c7416ab
José Alexandre Soares de Oliveira <91433859+hermengardo@users.noreply.github.com>
2026-06-03 12:51:28 -0300

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string longest = "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char target = strs[0][i];
+
+            for (int j = 1; j < std::size(strs); j++) {
+                if (strs[j][i] != target || std::size(strs[j]) < i) {
+                    return longest;
+                }
+            }
+
+            longest += target;
+        }
+
+        return longest;
+    }
+};
