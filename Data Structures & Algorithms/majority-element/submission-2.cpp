93bdbf857fb8929e3274f35c97c00fbe7840cc48
José Alexandre Soares de Oliveira <91433859+hermengardo@users.noreply.github.com>
2026-06-03 23:10:36 -0300

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        std::unordered_map<int, int> freqs;
+
+        int candidate;
+        int counter = 0;
+        int k = std::floor(std::size(nums) / 2);
+
+        for (int num : nums) {
+            if (counter == 0) {
+                candidate = num;
+            }
+
+            if (counter > k) {
+                return candidate;
+            }
+
+            if (num == candidate) {
+                counter++;
+            } else {
+                counter--;
+            }
+        }
+
+        return candidate;
+    }
+};
