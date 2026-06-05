35ca55648509360604da0b40c6fba6a69734b2e3
José Alexandre Soares de Oliveira <91433859+hermengardo@users.noreply.github.com>
2026-06-05 04:00:42 -0300

    Add: sort-an-array - submission-3

---
+++ Data Structures & Algorithms/sort-an-array/submission-3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        quicksort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+
+    void quicksort(vector<int>& nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int aux;
+        int i = start-1;
+        int ridx = start + std::rand() % (end - start + 1);
+        swap(nums, ridx, end);
+        int pivot = nums[end];
+
+        for (int j = start; j < end; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+
+        swap(nums, i+1, end);
+        
+        quicksort(nums, start, i);
+        quicksort(nums, i+2, end);
+    }
+
+    void swap(vector<int>& nums, int i, int j) {
+        int aux = nums[i];
+        nums[i] = nums[j];
+        nums[j] = aux;
+    }
+};
