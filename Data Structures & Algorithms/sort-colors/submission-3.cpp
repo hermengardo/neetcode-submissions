7f1a860a9075425dfc9b77d01745c15f3edea70c
José Alexandre Soares de Oliveira <91433859+hermengardo@users.noreply.github.com>
2026-06-05 22:59:32 -0300

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        quicksort(nums, 0, nums.size() - 1);
+    }
+
+    void quicksort(vector<int>& nums, int start, int end) {
+        if (start > end) {
+            return;
+        }
+
+        int i = start - 1;
+        int pivot = nums[end];
+        int equals = 0;
+
+        for (int j = start; j < end; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                swap(nums, i, j);
+            }
+
+            if (nums[j] == pivot) {
+                equals++;
+            }
+        }
+
+        // All elements are equal => The subarray is already sorted
+        if (equals == start - end) {
+            return;
+        }
+
+        swap(nums, i + 1, end);
+
+        quicksort(nums, i + 2, end);
+        quicksort(nums, start, i);
+    }
+
+    void swap(vector<int>& nums, int i, int j) {
+        int aux = nums[i];
+        nums[i] = nums[j];
+        nums[j] = aux;
+    }
+};
