45a6e51a109b57d998dd64b8da6521e42ba9e611
José Alexandre Soares de Oliveira <91433859+hermengardo@users.noreply.github.com>
2026-06-09 17:45:53 -0300

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string result;
+        for (string str : strs) {
+            result = result + str + ";";
+        }
+
+        return result;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> arr;
+        string str = "";
+        std::cout << s;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ';') {
+                arr.push_back(str);
+                str = "";
+            } else {
+                str = str + s[i];
+            }
+        }
+
+        return arr;
+    }
+
+};
