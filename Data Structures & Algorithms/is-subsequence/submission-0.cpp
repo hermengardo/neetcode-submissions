302de642017ecde4bf092adb51e41f27cea14f73
José Alexandre Soares de Oliveira <91433859+hermengardo@users.noreply.github.com>
2026-09-07 23:39:33 -0300

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+        
+        int n = s.length();
+        int m = t.length();
+
+        if (n > m) {
+            return false;
+        }
+
+        while (i < n && j < m) {
+            if (s[i] == t[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        if (i == n) {
+            return true;
+        }
+
+        return false;
+    }
+};
